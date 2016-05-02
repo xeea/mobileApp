06057cc5a2a85656ba496183eb4d6a9cc416f4d3
Ivelin <ivelinivanov1995@gmail.com>
2016-05-02 10:42:13 -0700

    Initial push

---
+++ www/js/controllers.js
@@ -0,0 +1,14 @@
+angular.module('app.controllers', [])
+  
+.controller('homeCtrl', function($scope) {
+
+})
+   
+.controller('page2Ctrl', function($scope) {
+
+})
+   
+.controller('page3Ctrl', function($scope) {
+
+})
+    
